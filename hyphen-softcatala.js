a1e4d07056c38bbbc8136d39dedd4607f94a91c2
jaumeortola <jaumeortola@gmail.com>
2019-11-04 22:33:44 +0000

    first commit

---
+++ hyphen-softcatala.js
@@ -0,0 +1,22 @@
+  var hyphenate=createHyphenator(hyphenationPatternsCa,{debug:true,hyphenChar:'_'});
+
+  function onChangeFunction() {
+    original_text = normalizeNFC(document.getElementById("text_to_hyphen").value);
+    hyphenated_text = hyphenate(original_text).replace(/(l·|ŀ)/g, "l").replace(/(L·|Ŀ)/g, "L");
+    countSyllables(hyphenated_text);
+    document.getElementById("result").innerHTML = hyphenated_text;
+    document.getElementById("count").innerHTML = countSyllables(hyphenated_text) + " síl·labes";
+  }
+
+  function countSyllables(s) {
+    words = s.split(/[^a-zàáèéìíòóùúäëïöüç·ñ'’ŀâêîôû]/i);
+    return words.filter(String).length;
+  }
+
+  function normalizeNFC(userText) {
+    var normalizedText = userText;
+    if (String.prototype.hasOwnProperty('normalize')) {
+      normalizedText = userText.normalize("NFC");
+    }
+    return normalizedText;
+  }
